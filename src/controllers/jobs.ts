d814493a76c1d69cf24a315e84eb608cada82562
Pedro Henrique Mota <motapedrofsp@gmail.com>
2023-08-14 20:54:47 -0300

    feat<controllers>: added jobs controllers

---
+++ src/controllers/jobs.ts
@@ -0,0 +1,24 @@
+// createJob getAllJobs, getJob, updateJob, deleteJob
+
+import express from 'express'
+import { Request, Response } from 'express'
+
+export const createJob = async (req:Request,res:Response)=>{
+    res.send('createJob is now working')
+}
+
+export const getAllJobs = async (req:Request, res:Response) =>{
+    res.send('getAllJobs is now working')
+}
+
+export const getJob = async (req:Request,res:Response)=>{
+    res.send('getJob is now working')
+}
+
+export const updateJob = async (req:Request, res:Response)=>{
+    res.send('updateJob is now working')
+}
+
+export const deletJob = async (req:Request, res:Response)=>{
+    res.send('deleteJob is now working')
+}
