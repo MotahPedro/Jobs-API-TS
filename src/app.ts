6705090b38d8c2f43b3e3c7e958b980de1bc2fb3
Pedro Henrique Mota <motapedrofsp@gmail.com>
2023-08-14 20:29:58 -0300

    feat: added port 3000 on app.ts

---
+++ src/app.ts
@@ -0,0 +1,44 @@
+import 'express-async-errors'
+import {Request,Response} from 'express'
+import express, {Express} from 'express'
+import dotenv from 'dotenv'
+
+dotenv.config()
+const app : Express = express()
+
+import {connectDB} from './db/connect'
+
+// Routers
+
+
+// Middlewares
+import { notFoundMiddleware } from './middleware/not-found'
+import {errorHandlerMiddleware} from './middleware/error-handler'
+
+app.use(express.json())
+
+// Extra packages
+
+// Base page
+app.get('/', (req: Request,res: Response)=>{
+    res.send('jobs api')
+})
+
+//routes
+
+app.use(notFoundMiddleware)
+app.use(errorHandlerMiddleware)
+
+const port = process.env.PORT || 3000
+
+const start = async () =>{
+    try {
+        await connectDB(process.env.MONGO_URL)
+        app.listen(port,()=>
+        console.log(`Server is listening on port ${port}...`))
+    } catch (error){
+        console.log(error)
+    }
+}
+
+start()
